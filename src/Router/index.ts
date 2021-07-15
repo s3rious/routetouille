87c6a6760950d75e723c9a9fa14fe25c1f5cbd94
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-07-16 00:07:32 +0300

    Export every `Router` flavour and part

---
+++ src/Router/index.ts
@@ -0,0 +1,11 @@
+export * from './Router'
+
+export * from './WithRoot'
+export * from './WithMap'
+export * from './WithActive'
+export * from './WithPathname'
+export * from './WithParams'
+export * from './WithSet'
+export * from './WithGoTo'
+export * from './WithHistory'
+export * from './Subscribable'
