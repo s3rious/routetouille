742fc93f7461456aef759097fa973d461625b188
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-07-16 00:06:28 +0300

    Added `Router/WithParams`

---
+++ src/Router/WithParams/WithParams.ts
@@ -0,0 +1,52 @@
+import { WithPathnameInterface, WithPathnameOptions } from '../WithPathname'
+import { Activator, WithActiveInterface, WithActiveOptions } from '../WithActive'
+
+type RouterComposedOptions = WithPathnameOptions & WithActiveOptions
+type RouterComposedInterface = WithPathnameInterface & WithActiveInterface
+
+type Params = Array<{ [key: string]: string }>
+
+type WithParamsOptions = {}
+type WithParamsInterface = WithParamsOptions & {
+  params: Params
+  activate: (activator: Activator, params: Params, optimistic?: boolean) => Promise<void>
+}
+
+function WithParams<ComposedOptions extends RouterComposedOptions, ComposedInterface extends RouterComposedInterface>(
+  createRouter?: (options: ComposedOptions) => ComposedInterface,
+) {
+  return function (options: WithParamsOptions & ComposedOptions): WithParamsInterface & ComposedInterface {
+    const composed: ComposedInterface = createRouter?.(options) ?? ({} as ComposedInterface)
+    const pathname = composed.pathname
+    const params: Params = []
+
+    async function activate(
+      this: WithParamsInterface & ComposedInterface,
+      activator: Activator,
+      params: Params = [],
+      optimistic?: boolean,
+    ): Promise<void> {
+      await composed.activate.bind(this)(activator, optimistic)
+      this.params = params
+
+      if (typeof this.pathname === 'string') {
+        let pathname = this.pathname
+
+        params.forEach((param) => {
+          const [key, value] = Object.entries(param)[0]
+          const nextPathname = pathname.replace(new RegExp(`:${key}`), value)
+
+          if (nextPathname !== '') {
+            pathname = nextPathname
+          }
+        })
+
+        this.pathname = pathname
+      }
+    }
+
+    return { ...composed, pathname, params, activate }
+  }
+}
+
+export { WithParams, WithParamsOptions, WithParamsInterface, Params }
