e34915cacb0ec3db3bd87f35e1826576f2d07fdb
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-07-18 00:59:56 +0300

    Added `components/atoms/Link/`

---
+++ examples/single-react/src/components/atoms/Link/Link.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import { Activator, Params, RouterInterface } from 'routetouille'
+
+import { RootContext } from '../../../modules/root/Root'
+
+type LinkProps = {
+  children: React.ReactNode
+  to: Activator
+  params?: Params
+  optimistic?: boolean
+} & React.HTMLProps<HTMLAnchorElement>
+
+function Link({ children, to, params, optimistic = true, ...rest }: LinkProps): React.ReactElement {
+  const router: RouterInterface | undefined = React.useContext(RootContext).router
+
+  const href: string | undefined = React.useMemo(() => {
+    if (Boolean(router)) {
+      return router.urlTo(to, params) ?? undefined
+    }
+
+    return undefined
+  }, [to, params, router])
+
+  const handleClick: React.MouseEventHandler<HTMLAnchorElement> = React.useCallback(
+    async (event) => {
+      if (Boolean(router)) {
+        event.preventDefault()
+
+        await router.goTo(to, { params, optimistic })
+      }
+    },
+    [to, params, optimistic, router],
+  )
+
+  return (
+    <a href={href} onClick={handleClick} {...rest}>
+      {children}
+    </a>
+  )
+}
+
+export { Link }
