0be72379726b2ae8b85108b73a09c243830e5780
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-07-22 22:29:51 +0300

    Added basic `effector-routetouille/Route/WithEffectorStore`

--- examples/single-react/src/router/routes/index.ts
+++ examples/single-react/src/router/routes/index.ts
@@ -12,29 +12,35 @@ import {
   WithReactComponentInterface,
   WithReactComponentOptions,
 } from 'local_modules/react-routetouille/Route'
+import { WithEffectorStoreInterface, WithEffectorStoreOptions } from 'local_modules/effector-routetouille/Route'
 
-type AnyRouteInterface =
-  | RouteInterface
-  | (WithReactRootInterface & RouteInterface)
-  | (WithReactComponentInterface & RouteInterface)
-  | ModuleRouteInterface
-  | (WithReactRootInterface & ModuleRouteInterface)
-  | (WithReactComponentInterface & ModuleRouteInterface)
-  | FallbackRouteInterface
-  | (WithReactRootInterface & FallbackRouteInterface)
-  | (WithReactComponentInterface & FallbackRouteInterface)
+type GenericRouteInterface<Interface, State extends unknown> =
+  | Interface
+  | (WithReactRootInterface & Interface)
+  | (WithReactComponentInterface & Interface)
+  | (WithEffectorStoreInterface<State> & Interface)
+  | (WithEffectorStoreInterface<State> & WithReactRootInterface & Interface)
+  | (WithEffectorStoreInterface<State> & WithReactComponentInterface & Interface)
 
-type AnyRouteOptions =
-  | RouteOptions
-  | (WithReactRootOptions & RouteOptions)
-  | (WithReactComponentOptions & RouteOptions)
-  | ModuleRouteOptions
-  | (WithReactRootOptions & ModuleRouteOptions)
-  | (WithReactComponentOptions & ModuleRouteOptions)
-  | FallbackRouteOptions
-  | (WithReactRootOptions & FallbackRouteOptions)
-  | (WithReactComponentOptions & FallbackRouteOptions)
+type GenericRouteOptions<Options, State extends unknown> =
+  | Options
+  | (WithReactRootOptions & Options)
+  | (WithReactComponentOptions & Options)
+  | (WithEffectorStoreOptions<State> & Options)
+  | (WithEffectorStoreOptions<State> & WithReactRootOptions & Options)
+  | (WithEffectorStoreOptions<State> & WithReactComponentOptions & Options)
+
+type AnyRouteInterface<State = unknown> =
+  | GenericRouteInterface<RouteInterface, State>
+  | GenericRouteInterface<ModuleRouteInterface, State>
+  | GenericRouteInterface<FallbackRouteInterface, State>
+
+type AnyRouteOptions<State = unknown> =
+  | GenericRouteOptions<RouteOptions, State>
+  | GenericRouteOptions<ModuleRouteOptions, State>
+  | GenericRouteOptions<FallbackRouteOptions, State>
 
 export * from 'routetouille/lib/Route'
 export * from 'local_modules/react-routetouille/Route'
+export * from 'local_modules/effector-routetouille/Route'
 export { AnyRouteInterface, AnyRouteOptions }
