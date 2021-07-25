5acfbefd20913efcf609728df78562ed7810cb8b
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-07-26 00:34:26 +0300

    Added `Card` atom component

---
+++ examples/single-react/src/components/atoms/Card/Card.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { ReactElement, ReactNode } from 'react'
+import classNames from 'classnames/dedupe'
+
+import styles from './Card.module.css'
+
+type CardProps = {
+  children: ReactNode
+  className?: string
+  level?: 1 | 2 | 3 | 4
+}
+
+function Card({ children, className, level = 1 }: CardProps): ReactElement {
+  return (
+    <div className={classNames(className, styles.Card, { [styles[`Card_level_${level}`]]: level })}>{children}</div>
+  )
+}
+
+export { Card, CardProps }
