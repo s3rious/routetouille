5435ea97f2b06fb017629514e322e370c6647c8d
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-07-25 23:35:52 +0300

    Added `Logo` atom component

---
+++ examples/single-react/src/components/atoms/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { ReactElement } from 'react'
+
+// import { Link } from 'components/atoms/Link'
+
+function Logo(): ReactElement {
+  return <div>⚛ Routetouille</div>
+  // return <Link to="root">⚛ Routetouille</Link>
+}
+
+export { Logo }
