6c345c9af5eb834300a31ab183ca891703a7d039
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-07-30 01:52:14 +0300

    Added `Stack` atom component

---
+++ examples/react/src/components/atoms/Stack/Stack.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import { ReactElement, ReactNode, ReactNodeArray } from 'react'
+import classNames from 'classnames/dedupe'
+
+import { Spacing, SpacingProps, SpacingSize } from 'components/atoms/Spacing'
+
+import styles from './Stack.module.css'
+
+type StackAligns = 'stretch' | 'start' | 'center' | 'end' | 'baseline'
+
+type GeneralStackProps = {
+  children?: ReactNode | ReactNodeArray
+  className?: string
+  align?: StackAligns
+  inline?: boolean
+}
+
+type VerticalStackProps = {
+  vertical: SpacingSize
+} & GeneralStackProps
+
+type HorizontalStackProps = {
+  horizontal: SpacingSize
+} & GeneralStackProps
+
+type StackProps = XOR<VerticalStackProps, HorizontalStackProps>
+
+function Stack({
+  children,
+  className,
+  vertical,
+  horizontal,
+  align = 'stretch',
+  inline = false,
+}: StackProps): ReactElement {
+  const classes = classNames(className, styles.Stack, {
+    [styles.Stack_vertical]: vertical,
+    [styles.Stack_horizontal]: horizontal,
+    [styles[`Stack_align_${align}`]]: align,
+    [styles.Stack_inline]: inline,
+  })
+
+  return (
+    <div className={classes}>
+      {React.Children.toArray(children)
+        .filter((child) => Boolean(child))
+        .map((child, index) => {
+          const isFirst = index === 0
+
+          if (isFirst) {
+            return child
+          }
+
+          if (vertical) {
+            return (
+              <Spacing key={index} top={vertical}>
+                {child}
+              </Spacing>
+            )
+          }
+
+          return (
+            <Spacing key={index} left={horizontal}>
+              {child}
+            </Spacing>
+          )
+        })}
+    </div>
+  )
+}
+
+export { Stack }
--- examples/react/src/global.d.ts
+++ examples/react/src/global.d.ts
@@ -7,3 +7,9 @@ declare module '*.module.scss' {
   const content: Record<string, string>
   export default content
 }
+
+type Without<T, U> = {
+  [P in Exclude<keyof T, keyof U>]?: never
+}
+
+type XOR<T, U> = T | U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U
