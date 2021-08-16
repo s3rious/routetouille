6cacdbd32e0a22d0ceefe3c4204e73ce2f7d2460
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-08-17 00:08:16 +0300

    Rework Fallback page to make it a bit more beautiful

--- examples/react/src/domains/fallback/components/Fallback/Fallback.tsx
+++ examples/react/src/domains/fallback/components/Fallback/Fallback.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react'
 import { ReactElement } from 'react'
 
+import { Layout } from 'components/atoms/Layout'
+import { AllCenter } from 'components/atoms/AllCenter'
+import { Stack } from 'components/atoms/Stack'
+import { Typography } from 'components/atoms/Typography'
+
 function Fallback(): ReactElement {
-  return <div>Fallback</div>
+  return (
+    <Layout
+      content={
+        <AllCenter>
+          <Stack vertical={4}>
+            <Typography size={128} lineHeight="small" weight="black" align="center">
+              404
+            </Typography>
+            <Typography size={20} lineHeight="small" align="center">
+              These aren't the page you're looking for...
+            </Typography>
+          </Stack>
+        </AllCenter>
+      }
+    />
+  )
 }
 
 export { Fallback }
