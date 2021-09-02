206f78b050fecbae92f4ebe3906c68b067b1f80a
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-09-03 01:39:51 +0300

    Added `jest` as a test runner

---
+++ examples/react/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^local_modules(.*)$': '<rootDir>/src/local_modules$1',
+    '^components(.*)$': '<rootDir>/src/components$1',
+    '^domains(.*)$': '<rootDir>/src/domains$1',
+    '^services(.*)$': '<rootDir>/src/services$1',
+  },
+}
