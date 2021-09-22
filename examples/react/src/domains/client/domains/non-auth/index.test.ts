3f8f4e3652fd46272b39d63bc2dd8d66b8b972b2
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2021-09-23 01:04:28 +0300

    Added `domains/client/domains/non-auth/index` test

---
+++ examples/react/src/domains/client/domains/non-auth/index.test.ts
@@ -0,0 +1,73 @@
+import { Router, Route } from 'services/router'
+
+import { getRoute } from './index'
+import { $client, $accessToken, effects } from 'domains/client'
+
+jest.mock('domains/client')
+
+const router = Router({})
+const nonAuthChildren1Route = Route({
+  name: 'children1',
+  path: 'children1/',
+})
+const nonAuthChildren2Route = Route({
+  name: 'children2',
+  path: 'children2/',
+})
+const nonAuthRoute = getRoute(router, [nonAuthChildren1Route, nonAuthChildren2Route])
+const authRoute = Route({
+  name: 'auth',
+  path: 'auth/',
+})
+const rootRoute = Route({
+  name: 'root',
+  path: '/',
+  children: [nonAuthRoute, authRoute],
+})
+router.root = rootRoute
+
+async function wait(timeout = 1) {
+  return await new Promise(function (resolve) {
+    setTimeout(resolve, timeout)
+  })
+}
+
+describe('when `accessToken` is set', function () {
+  beforeAll(async function () {
+    router.active = []
+    jest.clearAllMocks()
+
+    // @ts-expect-error
+    $accessToken.getState.mockImplementation(() => '123')
+
+    await router.goTo('non-auth')
+  })
+
+  afterEach(async function () {
+    await wait()
+  })
+
+  it('should redirect to `auth`', function () {
+    expect(router.active[router.active.length - 1].name).toEqual('auth')
+  })
+})
+
+describe('when `accessToken` is not set', function () {
+  beforeAll(async function () {
+    router.active = []
+    jest.clearAllMocks()
+
+    // @ts-expect-error
+    $accessToken.getState.mockImplementation(() => null)
+
+    await router.goTo('non-auth')
+  })
+
+  afterEach(async function () {
+    await wait()
+  })
+
+  it('should redirect to first children of `non-auth`', async function () {
+    expect(router.active[router.active.length - 1].name).toEqual('children1')
+  })
+})
